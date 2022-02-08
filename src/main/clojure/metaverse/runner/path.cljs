87927c18bfb2836a96e5553e9e87e3df49797ba6
Ilshat Sultanov <just.sultanov@gmail.com>
2022-02-08 22:54:02 +0300

    Add helper functions that work with paths

--- src/main/clojure/metaverse/runner/path.cljs
+++ src/main/clojure/metaverse/runner/path.cljs
@@ -1,1 +1,20 @@
-(ns metaverse.runner.path)
+(ns metaverse.runner.path
+  (:require
+    ["path" :as path]
+    [metaverse.runner.config :as config]))
+
+
+(defn join
+  [& paths]
+  (reduce
+    (fn [acc v]
+      (.join path acc v))
+    paths))
+
+
+(def assets-dir
+  (join config/root-dir "assets"))
+
+
+(def icons-dir
+  (join assets-dir "icons"))
