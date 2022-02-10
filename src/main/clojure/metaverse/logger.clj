1ac14ab43cab130990916ae6d38aab196b7b394f
Ilshat Sultanov <just.sultanov@gmail.com>
2022-02-11 02:59:58 +0300

    Add logger API

---
+++ src/main/clojure/metaverse/logger.clj
@@ -0,0 +1,28 @@
+(ns metaverse.logger
+  (:require
+    [lambdaisland.glogi :as log]))
+
+
+(defmacro error
+  [& keyvals]
+  (#'log/log-expr &form :error keyvals))
+
+
+(defmacro warn
+  [& keyvals]
+  (#'log/log-expr &form :warn keyvals))
+
+
+(defmacro info
+  [& keyvals]
+  (#'log/log-expr &form :info keyvals))
+
+
+(defmacro debug
+  [& keyvals]
+  (#'log/log-expr &form :debug keyvals))
+
+
+(defmacro trace
+  [& keyvals]
+  (#'log/log-expr &form :trace keyvals))
---
+++ src/main/clojure/metaverse/logger.cljs
@@ -0,0 +1,19 @@
+(ns metaverse.logger
+  (:require
+    [lambdaisland.glogi :as log]
+    [lambdaisland.glogi.console :as console]
+    [metaverse.env :as env]))
+
+
+(defn init!
+  "Logger initializer."
+  ([]
+   (init! env/logger-level))
+
+  ([logger-level]
+   (let [level (or logger-level :off)]
+     (console/install!)
+     (log/set-levels
+       {:glogi/root level
+        'metaverse  level})
+     (log/debug :msg "Logger successfully initialized" :level level))))
