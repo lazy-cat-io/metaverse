ff1dbd6511d72d92e37fc21abca5c3da4015b291
Ilshat Sultanov <just.sultanov@gmail.com>
2022-02-11 02:58:30 +0300

    Add env

---
+++ src/main/clojure/metaverse/env.cljs
@@ -0,0 +1,28 @@
+(ns metaverse.env
+  (:require
+    [clojure.string :as str]))
+
+
+(goog-define company-name "@team.sultanov")
+(goog-define product-name "metaverse")
+
+(goog-define mode "production")
+(goog-define version "N/A")
+(goog-define build-number "N/A")
+(goog-define build-timestamp "N/A")
+(goog-define git-url "N/A")
+(goog-define git-branch "N/A")
+(goog-define git-sha "N/A")
+(goog-define logger-level "error")
+(goog-define default-language "en")
+
+(goog-define sentry-dsn "N/A")
+
+
+
+(def develop?
+  (= "develop" (str/lower-case mode)))
+
+
+(def production?
+  (= "production" (str/lower-case mode)))
