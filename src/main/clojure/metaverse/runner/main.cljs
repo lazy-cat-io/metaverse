1d0a033b5de57b09931f53e24ec85af3c3f5b6c4
Ilshat Sultanov <just.sultanov@gmail.com>
2022-02-11 03:08:41 +0300

    Update main namespace

--- src/main/clojure/metaverse/runner/main.cljs
+++ src/main/clojure/metaverse/runner/main.cljs
@@ -1,9 +1,11 @@
 (ns metaverse.runner.main
   (:require
+    [metaverse.logger :as logger]
     [metaverse.runner.app :as app]
     [metaverse.runner.config :as config]
     [metaverse.runner.menu :as menu]
     [metaverse.runner.positioner :as positioner]
+    [metaverse.runner.reporter :as reporter]
     [metaverse.runner.tray :as tray]
     [metaverse.runner.window :as window]))
 
@@ -22,6 +24,7 @@
 
 
 (defn closed-handler
+  {:dev/before-load true}
   []
   (window/destroy!)
   (tray/destroy!))
@@ -39,16 +42,31 @@
     (app/quit)))
 
 
+(defn setup-tools!
+  []
+  (logger/init!)
+  (reporter/init!))
+
+
+;; TODO: [2022-02-11, ilshat@sultanov.team] Add a global shortcut to search for a library or docs / to create a new project?
+;; example: (keyboard/register-global-shortcut! "Alt+CommandOrControl+I" #(window/toggle-devtools window))
+
+(defn setup-global-shortcuts!
+  [_window])
+
+
 
 ;;
 ;; Mount root
 ;;
 
 (defn mount
+  {:dev/after-load true}
   []
   (let [window (window/create-window)
         menu   (menu/create-menu window)
         tray   (tray/create-tray window)]
+    (setup-global-shortcuts! window)
     (menu/set-application-menu menu)
     (window/set-instance! window)
     (window/load-app window)
@@ -63,7 +81,7 @@
 
 (defn init!
   []
-  #_(reporter/init-sentry!)
+  (setup-tools!)
   (app/on :ready mount)
   (app/on :window-all-closed window-all-closed-handler)
   (app/on :activate activate-handler))
