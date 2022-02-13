c26f89e897f95196dc57724392114fec461ebf33
Ilshat Sultanov <just.sultanov@gmail.com>
2022-02-14 00:47:14 +0300

    Refactor runner window namespace

--- src/main/clojure/metaverse/runner/window.cljs
+++ src/main/clojure/metaverse/runner/window.cljs
@@ -1,82 +1,12 @@
 (ns metaverse.runner.window
   (:require
-    [metaverse.env :as env]
-    [metaverse.runner.config :as config]
-    [metaverse.runner.electron :as electron]
-    [metaverse.runner.refs :as refs]))
-
-
-(defn get-instance
-  []
-  @refs/*window)
-
-
-(defn set-instance!
-  [window]
-  (reset! refs/*window window))
-
-
-(defn reset-instance!
-  []
-  (set-instance! nil))
-
-
-(defn destroy!
-  []
-  (reset-instance!))
-
-
-(defn load-url
-  [^js/electron.BrowserWindow window url]
-  (.loadURL window url))
+    [metaverse.electron.window :as window]
+    [metaverse.runner.config :as config]))
 
 
 (defn load-app
   [window]
-  (load-url window config/index-url))
-
-
-(defn on
-  [event ^js/electron.BrowserWindow window handler]
-  (.on window (name event) handler))
-
-
-(defn show
-  [^js/electron.BrowserWindow window]
-  (.show window))
-
-
-(defn hide
-  [^js/electron.BrowserWindow window]
-  (.hide window))
-
-
-(defn set-title
-  [^js/electron.BrowserWindow window title]
-  (.setTitle window title))
-
-
-(defn get-all-windows
-  []
-  (.getAllWindows electron/BrowserWindow))
-
-
-(defn recreate-window?
-  []
-  (zero? (count (get-all-windows))))
-
-
-(defn toggle-devtools
-  [^js/electron.BrowserWindow window]
-  (when env/develop?
-    (if (.. window -webContents (isDevToolsOpened))
-      (.. window -webContents (closeDevTools))
-      (.. window -webContents (openDevTools #js {:mode "detach"})))))
-
-
-(defn browser-window
-  [opts]
-  (electron/BrowserWindow. (clj->js opts)))
+  (window/load-url window config/index-url))
 
 
 (defn build-browser-window-options
@@ -96,12 +26,14 @@
    :titleBarStyle   config/title-bar-style
    :useContentSize  config/use-content-size?
    :width           config/width
-   :webPreferences  {:nodeIntegration config/node-integration?
-                     :webSecurity     config/web-security?
-                     :sandbox         config/sandbox?}})
+   :webPreferences  {:nodeIntegration    config/node-integration?
+                     :webSecurity        config/web-security?
+                     :sandbox            config/sandbox?
+                     :preload            config/preload-file}})
 
 
 (defn create-window
-  []
-  (-> (build-browser-window-options)
-      (browser-window)))
+  ([]
+   (window/browser-window (build-browser-window-options)))
+  ([opts]
+   (window/browser-window opts)))
