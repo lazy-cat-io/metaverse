6d2576b02f8502bd9a73daa7ed419e24f1cb4650
Ilshat Sultanov <just.sultanov@gmail.com>
2022-02-14 00:28:15 +0300

    Refactor runner reporter namespace

--- src/main/clojure/metaverse/runner/reporter.cljs
+++ src/main/clojure/metaverse/runner/reporter.cljs
@@ -1,17 +1,15 @@
 (ns metaverse.runner.reporter
   (:require
-    ["@sentry/electron" :as sentry]
     [metaverse.env :as env]
-    [metaverse.logger :as logger :include-macros true]))
+    [metaverse.logger :as log :include-macros true]
+    [metaverse.sentry :as sentry]))
 
 
 (defn init!
   []
   (when env/production?
     (if (= "N/A" env/sentry-dsn)
-      (logger/error :msg "Sentry reporter is not initialized" :sentry-dsn env/sentry-dsn)
+      (log/error :msg "Sentry reporter is not initialized" :sentry-dsn env/sentry-dsn)
       (do
-        (->> {:dsn env/sentry-dsn}
-             (clj->js)
-             (.init sentry))
-        (logger/info :msg "Sentry reporter successfully initialized" :sentry-dsn env/sentry-dsn)))))
+        (sentry/init! {:dsn env/sentry-dsn})
+        (log/info :msg "Sentry reporter successfully initialized" :sentry-dsn env/sentry-dsn)))))
