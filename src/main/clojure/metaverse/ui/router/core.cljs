61ff22bee09c42a952a23740253ac01dc9630ee4
Ilshat Sultanov <just.sultanov@gmail.com>
2022-02-17 04:09:40 +0300

    Change routes

--- src/main/clojure/metaverse/ui/router/core.cljs
+++ src/main/clojure/metaverse/ui/router/core.cljs
@@ -11,21 +11,21 @@
 
 (def routes
   [""
-   ["/" {:name :page/root, :private false}]
+   ["/" {:name :page/home, :private false}]
    ["/sign-in"
     ["/" {:name :page/sign-in, :private false}]
     ["/github" {:name :page/sign-in.github, :private false}]]
-   ["/explore"
-    ["" {:name :page/explore, :private false}]
-    ["/trends" {:name :page/explore.trends, :private false}]
-    ["/projects" {:name :page/explore.projects, :private false}]
-    ["/docs" {:name :page/explore.docs, :private false}]
-    ["/jobs" {:name :page/explore.jobs, :private false}]]
+   ["/news" {:name :page/news, :private false}]
+   ["/projects"
+    ["/" {:name :page/projects, :private false}]
+    ["/trends" {:name :page/projects.trends, :private false}]]
+   ["/jobs" {:name :page/jobs, :private false}]
+   ["/docs" {:name :page/docs, :private false}]
+   ["/initializer"
+    ["" {:name :page/initializer, :private false}]]
    ["/profile"
     ["/" {:name :page/profile, :private false}]
-    ["/settings" {:name :page/profile.settings, :private false}]]
-   ["/initializer"
-    ["" {:name :page/initializer, :private false}]]])
+    ["/settings" {:name :page/profile.settings, :private false}]]])
 
 
 (def router
