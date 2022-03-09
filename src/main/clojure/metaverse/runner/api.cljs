bd19ff0c1fbd64b50dbbdeb9475fbcb0836567af
Ilshat Sultanov <just.sultanov@gmail.com>
2022-03-10 02:35:27 +0300

    Remove unnecessary event handlers

--- src/main/clojure/metaverse/runner/api.cljs
+++ src/main/clojure/metaverse/runner/api.cljs
@@ -4,25 +4,10 @@
 
 
 (defmulti dispatch
-  (fn [_ipc-event command event]
-    (log/info "dispatch" command "event" event)
+  (fn [_ipc-event command _event]
     (or command :default)))
 
 
 (defmethod dispatch :default
   [_ command event]
   (log/error "Unknown command" command "event" event))
-
-
-(defmethod dispatch :increment
-  [_ _ n]
-  (js/Promise.
-    (fn [resolve _]
-      (resolve (inc n)))))
-
-
-(defmethod dispatch :decrement
-  [_ _ n]
-  (js/Promise.
-    (fn [resolve _]
-      (resolve (dec n)))))
