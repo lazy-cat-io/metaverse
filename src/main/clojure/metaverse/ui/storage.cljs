f2ac5c6f4d0acec0332eb8c9a5ce3c54d8da8cdc
Ilshat Sultanov <just.sultanov@gmail.com>
2022-03-10 02:32:17 +0300

    Add helper functions working with localStorage

--- src/main/clojure/metaverse/ui/storage.cljs
+++ src/main/clojure/metaverse/ui/storage.cljs
@@ -1,31 +1,76 @@
 (ns metaverse.ui.storage
   (:require
-    [cljs.reader :as reader]
+    [cljs-bean.core :as bean]
+    [metaverse.utils.string :refer [keyword->string]]
     [re-frame.core :as rf]))
 
 
-;; TODO: [2022-02-22, ilshat@sultanov.team] serialize/deserialize using clj->js and js->clj
-
 (defn set-item!
-  [key value]
-  (.setItem js/localStorage (name key) value))
+  [key item]
+  (when-some [key (keyword->string key)]
+    (let [item (.stringify js/JSON (bean/->js item))]
+      (.setItem js/localStorage key item))))
+
+
+(defn set-items!
+  [pairs]
+  (run! set-item! pairs))
+
 
 
 (defn get-item
   [key]
-  (-> (.getItem js/localStorage (name key))
-      (reader/read-string)))
+  (when-some [key (keyword->string key)]
+    (let [item (.getItem js/localStorage key)]
+      (bean/->clj (.parse js/JSON item)))))
+
+
+(defn get-items
+  [ks]
+  (reduce
+    (fn [acc key]
+      (assoc acc key (get-item key)))
+    {} ks))
+
 
 
 (defn remove-item!
   [key]
-  (.removeItem js/localStorage (name key)))
+  (when-some [key (keyword->string key)]
+    (.removeItem js/localStorage key)))
+
+
+(defn remove-items!
+  [ks]
+  (run! remove-item! ks))
 
 
 
 (rf/reg-cofx
-  :local-storage
+  :local-storage/get-item
   (fn [cofx key]
     (->> key
          (get-item)
          (assoc-in cofx [:local-storage key]))))
+
+
+(rf/reg-cofx
+  :local-storage/get-items
+  (fn [cofx ks]
+    (->> ks
+         (get-items)
+         (assoc cofx :local-storage))))
+
+
+
+(rf/reg-fx
+  :local-storage/set-item
+  (fn [[key item]]
+    (set-item! key item)))
+
+
+(rf/reg-fx :local-storage/set-items set-items!)
+
+
+(rf/reg-fx :local-storage/remove-item remove-item!)
+(rf/reg-fx :local-storage/remove-items remove-items!)
