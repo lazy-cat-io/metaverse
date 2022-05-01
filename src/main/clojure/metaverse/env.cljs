fcb2e8a3fef40751220dd7d25cc62974e8431beb
Ilshat Sultanov <ilshat@sultanov.team>
2022-05-02 02:24:44 +0300

    Set default logger level to `info`

--- src/main/clojure/metaverse/env.cljs
+++ src/main/clojure/metaverse/env.cljs
@@ -10,13 +10,7 @@
 (goog-define product-name "metaverse")
 
 (goog-define mode "production")
-(goog-define version "N/A")
-(goog-define build-number "N/A")
-(goog-define build-timestamp "N/A")
-(goog-define git-url "N/A")
-(goog-define git-branch "N/A")
-(goog-define git-sha "N/A")
-(goog-define logger-level "error")
+(goog-define logger-level "info")
 (goog-define default-language "en")
 
 (goog-define sentry-dsn "N/A")
