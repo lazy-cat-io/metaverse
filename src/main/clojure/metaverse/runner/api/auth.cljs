c9eeb189299db1a8fc42e7835fbfe2e25dadb210
Ilshat Sultanov <ilshat@sultanov.team>
2022-05-03 17:17:25 +0300

    Update auth API

--- src/main/clojure/metaverse/runner/api/auth.cljs
+++ src/main/clojure/metaverse/runner/api/auth.cljs
@@ -3,7 +3,8 @@
     [applied-science.js-interop :as j]
     [metaverse.logger :as log :include-macros true]
     [metaverse.runner.api :as api]
-    [metaverse.supabase :as supabase]))
+    [metaverse.supabase :as supabase]
+    [tenet.response :as r]))
 
 
 (defmulti sign-in
@@ -11,58 +12,79 @@
     (cond
       provider :provider
       (and email password) :email
-      :else :default)))
+      :else :incorrect)))
 
 
-;; TODO: [2022-03-10, ilshat@sultanov.team] return error?
-(defmethod sign-in :default
-  [_ _ opts]
-  (log/error "Unknown provider" opts))
+(defmethod sign-in :incorrect
+  [_]
+  (let [message "Unknown provider"]
+    (log/error :msg message)
+    (js/Promise.
+      (fn [resolve _reject]
+        (resolve (r/as-incorrect {:message message}))))))
+
 
 
 (defmethod sign-in :email
   [{:keys [email password]}]
-  (js/Promise.
-    (fn [resolve reject]
-      (-> (supabase/auth:sign-in {:email email, :password password})
-          (.then (fn [response]
-                   (log/info :sign-in response)
-                   (resolve (supabase/auth:user))))
-          (.catch (fn [error]
-                    (log/error :sign-in error)
-                    (reject error)))))))
+  (-> {:email email, :password password}
+      (supabase/auth:sign-in)
+      (.then (fn [res]
+               (if (r/anomaly? res)
+                 res
+                 (update res :data #(j/get % :data)))))))
 
 
 (defmethod sign-in :provider
   [{:keys [provider]}]
-  (js/Promise.
-    (fn [resolve reject]
-      (-> (supabase/auth:sign-in {:provider provider})
-          (.then (fn [response]
-                   (if-some [url (j/get response :url)]
-                     (log/info :sign-in response)
-                     (log/error :sign-in response))))
-          (.catch (fn [error]
-                    (log/error :sign-in error)
-                    (reject error)))))))
+  (-> {:provider provider}
+      (supabase/auth:sign-in)
+      (.then (fn [res]
+               (if (r/anomaly? res)
+                 res
+                 ;; TODO: [2022-05-03, ilshat@sultanov.team] Open a window for signing in using the provider
+                 (update res :data #(j/select-keys % [:url :provider])))))))
 
 
 (defn sign-out
   []
-  (js/Promise.
-    (fn [resolve reject]
-      (-> (supabase/auth:sign-out)
-          (.then (fn [response]
-                   (log/info :sign-out response)
-                   (resolve response)))
-          (.catch (fn [error]
-                    (log/error :sign-out error)
-                    (reject error)))))))
+  (-> (supabase/auth:sign-out)
+      (.then (fn [_]
+               (r/as-success)))))
 
 
 ;;
 ;; Public API
 ;;
 
-(defmethod api/dispatch :auth/sign-in [_ _ opts] (sign-in opts))
-(defmethod api/dispatch :auth/sign-out [_ _ _] (sign-out))
+(defmethod api/dispatch :auth/sign-in [_ [_ credentials]] (sign-in credentials))
+(defmethod api/dispatch :auth/sign-out [_ _] (sign-out))
+
+
+
+(comment
+  ;; sign-up
+  (-> (supabase/auth:sign-up {:email "john@doe", :password "p4$$w0rd"})
+      (.then (fn [res]
+               (log/info :sign-up res))))
+
+  ;; wrong password
+  (-> (sign-in {:email "john@doe", :password "p4$$w0rd1"})
+      (.then (fn [res]
+               (log/info :sign-in res))))
+
+  ;; correct password
+  (-> (sign-in {:email "john@doe", :password "p4$$w0rd"})
+      (.then (fn [res]
+               (log/info :sign-in res))))
+
+  ;; sign-out
+  (-> (sign-out)
+      (.then (fn [res]
+               (log/info :sign-out res))))
+
+  ;; sign-in via github
+  (-> (sign-in {:provider "github"})
+      (.then (fn [res]
+               (log/info :sign-in res))))
+  )
