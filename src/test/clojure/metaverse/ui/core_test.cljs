1219657cc9d1201436b5afe64cab363f4619e343
Ilshat Sultanov <ilshat@sultanov.team>
2022-05-03 17:23:21 +0300

    Remove unnecessary tests

--- src/test/clojure/metaverse/ui/core_test.cljs
+++
@@ -1,9 +0,0 @@
-(ns metaverse.ui.core-test
-  (:require
-    [cljs.test :refer [deftest testing is]]
-    [metaverse.ui.core :as sut]))
-
-
-(deftest square-test
-  (testing "dummy test"
-    (is (= 4 (sut/square 2)))))
