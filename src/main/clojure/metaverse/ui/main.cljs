75043c07336ca2bea0d9e2223ed9d79ce52767e7
Ilshat Sultanov <ilshat@sultanov.team>
2022-05-05 03:49:19 +0300

    Add `dispatch` handler (handle events from main process)

--- src/main/clojure/metaverse/ui/main.cljs
+++ src/main/clojure/metaverse/ui/main.cljs
@@ -6,7 +6,9 @@
     [metaverse.ui.pages.home :as home]
     [metaverse.ui.router.core :as router]
     [metaverse.ui.storage]
+    [metaverse.utils.transit :as t]
     [re-frame.core :as rf]
+    [reagent.core :as r]
     [reagent.dom :as dom]))
 
 
@@ -18,7 +20,16 @@
 
 (defn root
   []
-  [home/page])
+  (let [did-mount (fn [_]
+                    (.. js/window -bridge
+                        (dispatch (fn [_ipc-event event]
+                                    (let [event (t/read event)]
+                                      (rf/dispatch @event))))))]
+    (r/create-class
+      {:display-name        "root"
+       :component-did-mount did-mount
+       :reagent-render      (fn []
+                              [home/page])})))
 
 
 (defn mount-root
