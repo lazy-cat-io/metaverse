472e6f02560d425d0d0eda8eec8170e3d03a2066
Ilshat Sultanov <ilshat@sultanov.team>
2022-05-05 03:39:48 +0300

    Add communication from main to renderer

--- public/preload.js
+++ public/preload.js
@@ -1,5 +1,12 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("bridge", {
-  dispatch: (event) => ipcRenderer.invoke("dispatch", event),
+  // Renderer to main (one-way)
+  send: (event) => ipcRenderer.send("send", event),
+
+  // Renderer to main (two-way)
+  invoke: (event) => ipcRenderer.invoke("invoke", event),
+
+  // Main to renderer
+  dispatch: (event) => ipcRenderer.on("dispatch", event),
 });
