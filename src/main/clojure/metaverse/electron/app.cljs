d39840a2231094c8f65942c50cddc97801a1c16b
Ilshat Sultanov <ilshat@sultanov.team>
2022-05-05 03:41:02 +0300

    Add `request-single-instance-lock` and `set-as-default-protocol-client` helper functions

--- src/main/clojure/metaverse/electron/app.cljs
+++ src/main/clojure/metaverse/electron/app.cljs
@@ -5,7 +5,7 @@
 
 (defn on
   [event handler]
-  (.on electron/App (name event) handler))
+  (.on electron/App event handler))
 
 
 (defn dock-hide
@@ -16,3 +16,36 @@
 (defn quit
   []
   (.quit electron/App))
+
+
+(defn request-single-instance-lock
+  []
+  (.requestSingleInstanceLock electron/App))
+
+
+(defn set-as-default-protocol-client
+  [protocol]
+  (.setAsDefaultProtocolClient electron/App protocol))
+
+
+;; TODO: [2022-05-05, ilshat@sultanov.team] Implement the code below
+
+;; if (process.defaultApp) {
+;;   if (process.argv.length >= 2) {
+;;     app.setAsDefaultProtocolClient('electron-fiddle', process.execPath, [path.resolve(process.argv[1])])
+;;   }
+;; } else {
+;;   app.setAsDefaultProtocolClient('electron-fiddle')
+;; }
+
+;; if (!gotTheLock) {
+;;  app.quit()
+;; } else {
+;;   app.on('second-instance', (event, commandLine, workingDirectory) => {
+;;    // Someone tried to run a second instance, we should focus our window.
+;;    if (mainWindow) {
+;;    if (mainWindow.isMinimized())
+;;      mainWindow.restore()
+;;      mainWindow.focus()
+;;     }
+;; })
